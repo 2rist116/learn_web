f23cabadd766fc9f2a7ff10b5a9bf65df72fb106
Vitaliy Inyushev <vinyushev@mail.ru>
2022-06-22 19:16:14 +0300

    исправил конфиг

--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,12 @@
 from datetime import timedelta
+import os
 
-REMEMBER_COOKIE_DURATION = timedelta(deys=5)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+WEATHER_DEFAULT_CITY = "Moscow,Russia"
+WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+WEATHER_API_KEY = '6319abdc786d470ab78171232222905'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+SECRET_KEY = "slgjn9482jto2mf2lkmed19-139"
+
+REMEMBER_COOKIE_DURATION = timedelta(days=5)
